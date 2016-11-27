1e4d23c0a7bb93c8c75c572e5050bb7ed44b713f
Grzegorz Gutkowski <gutkowski.grzegorz@gmail.com>
2016-11-27 17:09:46 +0100

    Some fixes

--- post.php
+++ post.php
@@ -1,19 +1,19 @@
 <?php
-    
+
     if($_POST) {
-        
+
         if($_POST['accept']) {
             $accept = "Wyrażam zgodę na przesyłanie informacji drogą elektroniczną.";
         } else {
             $accept = "Nie wyrażam zgody na przesyłanie informacji drogą elektroniczną.";
         }
-        
+
         $name = $_POST['name'];
         $email = $_POST['email'];
         $info = $_POST['company']." ".$_POST['city'].", ".$_POST['street']."<br>";
         $person = $_POST['name']." tel. ".$_POST['phone']."<br><br>";
-        
-        $to      = 'krzysztof.parasiak@vamergo.pl';
+
+        $to      = 'admin@example.com';
         $subject = $_POST['subject'];
         $message = $info.$person.$_POST['message']."<br>___<br>".$accept;
         $headers = 'MIME-Version: 1.0' . "\r\n" .
@@ -21,9 +21,9 @@
             'Reply-To: ' . $email . "\r\n" .
             'Content-type: text/html; charset=utf-8' . "\r\n" .
             'X-Mailer: PHP/' . phpversion();
-            
+
         mail($to, $subject, $message, $headers);
-    
+
     }
-    
+
 ?>
